70a7e5f466ed3a84b377ab8c3de6249e1487fb84
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-01-29 11:56:01 +0100

    dspsr pipeline draft

---
+++ mpikat/effelsberg/edd/pipeline/edd_worker_server.py
@@ -0,0 +1,291 @@
+import logging
+import tornado
+import coloredlogs
+import signal
+import json
+import os
+from optparse import OptionParser
+from tornado.gen import Return, coroutine
+from tornado.iostream import IOStream
+from katcp import AsyncDeviceServer, Sensor, ProtocolFlags, AsyncReply
+from katcp.kattypes import request, return_reply, Int, Str, Discrete, Float
+from mpikat.effelsberg.edd.pipeline import PIPELINE
+
+
+log = logging.getLogger("mpikat.edd.pipeline.edd_worker_server")
+
+#PIPELINES = {"mock":mock.Mock()}
+
+#P8_IP="10.17.8.2"
+
+class PafWorkerServer(AsyncDeviceServer):
+    """
+    @brief Interface object which accepts KATCP commands
+
+    """
+    VERSION_INFO = ("mpikat-paf-api", 1, 0)
+    BUILD_INFO = ("mpikat-paf-implementation", 0, 1, "rc1")
+    DEVICE_STATUSES = ["ok", "degraded", "fail"]
+    PIPELINE_STATES = ["idle", "configuring", "ready",
+                   "starting", "running", "stopping",
+                   "deconfiguring", "error"]
+
+    def __init__(self, ip, port):
+        """
+        @brief Initialization of the PafWorkerServer object
+
+        @param ip       IP address of the server
+        @param port     port of the PafWorkerServer
+
+        """
+        super(PafWorkerServer, self).__init__(ip, port)
+        self.ip = ip
+
+    def state_change(self, state, callback):
+        """
+        @brief callback function for state changes
+
+        @parma callback object return from the callback function from the pipeline
+        """
+        log.info('New state of the pipeline is {}'.format(str(state)))
+        self._pipeline_sensor_status.set_value(str(state))
+
+        #do I do things here?
+        """
+        if callback.state=="error":
+            @coroutine
+            def stop_pipeline():
+                try:
+                    self._pipeline_instance.stop()
+                except Exception as error:
+                    msg = "Couldn't stop pipeline {}".format(error)
+                    log.info("{}".format(msg))
+                    req.reply("fail", msg)
+                    self._pipeline_sensor_status.set_value("error")
+                    raise error
+                msg = "Stop pipeline {}".format(self._pipeline_sensor_name.value())
+                log.info("{}".format(msg))
+                req.reply("ok", msg)
+            self.ioloop.add_callback(configure_pipeline)
+            raise AsyncReply
+        """
+    def start(self):
+        super(PafWorkerServer, self).start()
+
+    def setup_sensors(self):
+        """
+        @brief Setup monitoring sensors
+        """
+        self._device_status = Sensor.discrete(
+            "device-status",
+            "Health status of PafWorkerServer",
+            params=self.DEVICE_STATUSES,
+            default="ok",
+            initial_status=Sensor.UNKNOWN)
+        self.add_sensor(self._device_status)
+
+        self._pipeline_sensor_name = Sensor.string("pipeline-name",
+            "the name of the pipeline", "")
+        self.add_sensor(self._pipeline_sensor_name)
+
+        self._pipeline_sensor_status = Sensor.discrete(
+            "pipeline-status",
+            description="Status of the pipeline",
+            params=self.PIPELINE_STATES,
+            default="idle",
+            initial_status=Sensor.UNKNOWN)
+        self.add_sensor(self._pipeline_sensor_status)
+
+        self._ip_address = Sensor.string("ip",
+            description="the ip address of the node controller",
+            default=os.environ['PAF_NODE_IP'])
+        self.add_sensor(self._ip_address)
+
+        self._mac_address = Sensor.string("mac",
+            description="the mac address of the node controller",
+            default=os.environ['PAF_NODE_MAC'])
+        self.add_sensor(self._mac_address)
+
+
+    @request(Str(),Str(),Str(),Str())
+    @return_reply(Str())
+    def request_configure(self, req, pipeline_name, utc_start, freq, ip):
+        """
+        @brief      Configure pipeline
+
+        @param      pipeline    name of the pipeline
+        """
+        @coroutine
+        def configure_pipeline():
+            self._pipeline_sensor_name.set_value(pipeline_name)
+            log.info("Configuring pipeline {}".format(self._pipeline_sensor_name.value()))
+            try:
+                _pipeline_type = PIPELINE[self._pipeline_sensor_name.value()]
+            except KeyError as error:
+                msg = "No pipeline called '{}', available pipeline are: \n{}".format(self._pipeline_sensor_name.value(), "\n".join(PIPELINES.keys()))
+                log.info("{}".format(msg))
+                req.reply("fail", msg)
+                self._pipeline_sensor_status.set_value("error")
+                self._pipeline_sensor_name.set_value("")
+                raise error
+            self._pipeline_instance = _pipeline_type()
+            self._pipeline_instance.callbacks.add(self.state_change)
+            try:
+                self._pipeline_instance.configure(utc_start, freq, str(self._ip_address.value()))
+            except Exception as error:
+                msg = "Couldn't start configure pipeline instance {}".format(str(error))
+                log.info("{}".format(msg))
+                req.reply("fail", msg)
+                self._pipeline_sensor_status.set_value("error")
+                self._pipeline_sensor_name.set_value("")
+                raise error
+            msg = "pipeline instance configured"
+            log.info("{}".format(msg))
+            req.reply("ok",msg)
+        if self._pipeline_sensor_status.value() == "idle":
+            self.ioloop.add_callback(configure_pipeline)
+            raise AsyncReply
+        else:
+            msg = "Can't Configure, status = {}".format(self._pipeline_sensor_status.value())
+            log.info("{}".format(msg))
+            return ("fail", msg)
+
+    @request(Str(),Str(),Str(),Str())
+    @return_reply(Str())
+    def request_start(self, req, source_name, ra, dec, start_buf):
+        """
+        @brief      Start pipeline
+
+        """
+        @coroutine
+        def start_pipeline():
+            try:
+                self._pipeline_instance.start(source_name, ra, dec, start_buf)
+            except Exception as error:
+                msg = "Couldn't start pipeline server {}".format(error)
+                log.info("{}".format(msg))
+                req.reply("fail", msg)
+                self._pipeline_sensor_status.set_value("error")
+                raise error
+            msg = "Start pipeline {}".format(self._pipeline_sensor_name.value())
+            log.info("{}".format(msg))
+            req.reply("ok", msg)
+
+        if self._pipeline_sensor_status.value() == "ready":
+            self.ioloop.add_callback(start_pipeline)
+            raise AsyncReply
+        else:
+            msg = "pipeline is not in the state of configured, status = {} ".format(self._pipeline_sensor_status.value())
+            log.info("{}".format(msg))
+            return ("fail", msg)
+
+    @request()
+    @return_reply(Str())
+    def request_stop(self, req):
+        """
+        @brief      Stop pipeline
+
+        """
+        @coroutine
+        def stop_pipeline():
+            self._pipeline_sensor_status.set_value("stopping")
+            try:
+                self._pipeline_instance.stop()
+            except Exception as error:
+                msg = "Couldn't stop pipeline {}".format(error)
+                log.info("{}".format(msg))
+                req.reply("fail", msg)
+                self._pipeline_sensor_status.set_value("error")
+                raise error
+            msg = "Stop pipeline {}".format(self._pipeline_sensor_name.value())
+            log.info("{}".format(msg))
+            req.reply("ok", msg)
+
+        if self._pipeline_sensor_status.value() == "running":
+            self.ioloop.add_callback(stop_pipeline)
+            raise AsyncReply
+        else :
+            msg = "nothing to stop, status = {}".format(self._pipeline_sensor_status.value())
+            log.info("{}".format(msg))
+            return ("fail", msg)
+
+    @request()
+    @return_reply(Str())
+    def request_deconfigure(self, req):
+        """
+        @brief      Deconfigure pipeline
+
+        """
+        @coroutine
+        def deconfigure():
+            log.info("deconfiguring pipeline {}".format(self._pipeline_sensor_name.value()))
+            try:
+                self._pipeline_instance.deconfigure()
+            except Exception as error:
+                msg = "Couldn't deconfigure pipeline {}".format(error)
+                log.info("{}".format(msg))
+                req.reply("fail", msg)
+                self._pipeline_sensor_status.set_value("error")
+                raise error
+            msg = "deconfigured pipeline {}".format(self._pipeline_sensor_name.value())
+            log.info("{}".format(msg))
+            req.reply("ok", msg)
+            self._pipeline_sensor_name.set_value("")
+        if self._pipeline_sensor_status.value() == "ready":
+            self.ioloop.add_callback(deconfigure)
+            raise AsyncReply
+        else:
+            msg = "nothing to deconfigure, status = {}".format(self._pipeline_sensor_status.value())
+            log.info("{}".format(msg))
+            return ("fail", msg)
+
+
+    @request()
+    @return_reply(Str())
+    def request_avail_pipeline(self, req):
+        """
+        @brief Get availiable pipelines
+        """
+        log.info("Requesting list of available pipeline")
+        for key in PIPELINES.keys():
+            req.inform("{}".format(key))
+        return ("ok", len(PIPELINES))
+
+
+@coroutine
+def on_shutdown(ioloop, server):
+    log.info('Shutting down server')
+    yield server.stop()
+    ioloop.stop()
+
+def main():
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-H', '--host', dest='host', type=str,
+        help='Host interface to bind to')
+    parser.add_option('-p', '--port', dest='port', type=long,
+        help='Port number to bind to')
+    parser.add_option('', '--log_level',dest='log_level',type=str,
+        help='logging level',default="INFO")
+    (opts, args) = parser.parse_args()
+    logging.getLogger().addHandler(logging.NullHandler())
+    logger = logging.getLogger('mpikat')
+    coloredlogs.install(
+        fmt="[ %(levelname)s - %(asctime)s - %(name)s - %(filename)s:%(lineno)s] %(message)s",
+        level=opts.log_level.upper(),
+        logger=logger)
+    logging.getLogger('katcp').setLevel('INFO')
+    ioloop = tornado.ioloop.IOLoop.current()
+    log.info("Starting PafWorkerServer instance")
+    server = PafWorkerServer(opts.host, opts.port)
+    signal.signal(signal.SIGINT, lambda sig, frame: ioloop.add_callback_from_signal(
+        on_shutdown, ioloop, server))
+    def start_and_display():
+        server.start()
+        log.info("Listening at {0}, Ctrl-C to terminate server".format(server.bind_address))
+
+    ioloop.add_callback(start_and_display)
+    ioloop.start()
+
+if __name__ == "__main__":
+    main()
---
+++ mpikat/effelsberg/edd/pipeline/pipeline.py
@@ -0,0 +1,244 @@
+import logging
+import tempfile
+import os
+import time
+import shutil
+from datetime import datetime
+#from docker.errors import APIError
+#from reynard.pipelines import Pipeline, reynard_pipeline
+#from reynard.dada import render_dada_header, make_dada_key_string
+
+log = logging.getLogger("reynard.DspsrPipeline")
+
+#
+# NOTE: For this to run properly the host /tmp/
+# directory should be mounted onto the launching container.
+# This is needed as docker doesn't currently support
+# container to container file copies.
+#
+
+PIPELINES = {}
+
+DESCRIPTION = """
+This pipeline captures data from the network and passes it to a dada
+ring buffer for processing by DSPSR
+""".lstrip()
+
+
+def register_pipeline(name):
+    def _register(cls):
+        PIPELINES[name] = cls
+        return cls
+    return _register
+
+
+@register_pipeline("DspsrPipeline")
+class Udp2Db2Dspsr(object):
+
+    def __del__(self):
+        class_name = self.__class__.__name__
+
+    def notify(self):
+        for callback in self.callbacks:
+            callback(self._state, self)
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        self._state = value
+        self.notify()
+
+    ulimits = [{
+        "Name": "memlock",
+        "Hard": -1,
+        "Soft": -1
+    }]
+
+    def __init__(self):
+        self.callbacks = set()
+        self._state = "idle"   # Idle at the very beginning
+        self._volumes = ["/tmp/:/scratch/"]
+        self._dada_key = None
+        self._config = None
+
+    def configure(self, config, sensors):
+        pass
+        self._config = config
+        self._dada_key = config["dada_db_params"]["key"]
+        try:
+            self._deconfigure()
+        except Exception:
+            pass
+        cmd = "dada_db -k {key} {args}".format(**config["dada_db_params"])
+        log.debug("Running command: {0}".format(cmd))
+        # replace with subprocess
+        self._docker.run(
+            self._config["dada_db_params"]["image"],
+            cmd, remove=True,
+            ipc_mode="host",
+            ulimits=self.ulimits)
+
+    def start(self, sensors):
+        header = self._config["dada_header_params"]
+        header["ra"] = sensors["ra"]
+        header["dec"] = sensors["dec"]
+        source_name = sensors["source-name"]
+        try:
+            source_name = source_name.split("_")[0]
+        except Exception:
+            pass
+        header["source_name"] = source_name
+        header["obs_id"] = "{0}_{1}".format(
+            sensors["scannum"], sensors["subscannum"])
+
+        dada_header_file = tempfile.NamedTemporaryFile(
+            mode="w",
+            prefix="reynard_dada_header_",
+            suffix=".txt",
+            dir="/scratch/",
+            delete=False)
+        log.debug(
+            "Writing dada header file to {0}".format(
+                dada_header_file.name))
+        header_string = render_dada_header(header)
+        dada_header_file.write(header_string)
+        log.debug("Header file contains:\n{0}".format(header_string))
+        dada_key_file = tempfile.NamedTemporaryFile(
+            mode="w",
+            prefix="reynard_dada_keyfile_",
+            suffix=".key",
+            dir="/scratch/",
+            delete=False)
+        log.debug("Writing dada key file to {0}".format(dada_key_file.name))
+        key_string = make_dada_key_string(self._dada_key)
+        dada_key_file.write(make_dada_key_string(self._dada_key))
+        log.debug("Dada key file contains:\n{0}".format(key_string))
+        dada_header_file.close()
+        dada_key_file.close()
+
+        self._set_watchdog("dspsr", callback=self.stop)
+        self._set_watchdog("udp2db")
+        self._set_watchdog("psrchive")
+
+        ###################
+        # Start up DSPSR
+        ###################
+        tstr = sensors["timestamp"].replace(":", "-")  # to fix docker bug
+        out_path = os.path.join("/output/", source_name, tstr)
+        host_out_path = os.path.join(self._config["base_output_dir"],
+                                     source_name, tstr)
+        volumes = ["/tmp/:/scratch/",
+                   "{}:/output/".format(
+                       self._config["base_output_dir"])]
+
+        # Make output directories via container call
+        log.debug("Creating directories")
+        cmd = "mkdir -p {}".format(out_path)
+        log.debug(cmd)
+        # replace with subprocess
+        self._docker.run(
+            self._config["dspsr_params"]["image"],
+            cmd,
+            volumes=volumes,
+            remove=True)
+
+        cmd = "dspsr {args} -N {source_name} {keyfile}".format(
+            args=self._config["dspsr_params"]["args"],
+            source_name=source_name,
+            keyfile=dada_key_file.name)
+        log.debug("Running command: {0}".format(cmd))
+        # replace with subprocess
+        self._docker.run(
+            self._config["dspsr_params"]["image"],
+            cmd,
+            detach=True,
+            name="dspsr",
+            ipc_mode="host",
+            volumes=volumes,
+            working_dir=out_path,
+            ulimits=self.ulimits,
+            requires_nvidia=True)
+
+        ############################
+        # Start up PSRCHIVE monitor
+        ############################
+
+        host_out_dir = os.path.join(
+            self._config["base_monitor_dir"], "timing", source_name, tstr)
+        out_dir = os.path.join("/output/timing/", source_name, tstr)
+        log.debug("Creating directory: {}".format(out_dir))
+        # replace with subprocess
+        self._docker.run(
+            self._config["psrchive_params"]["image"],
+            "mkdir -p {}".format(out_dir),
+            volumes=["{}:/output/".format(self._config["base_monitor_dir"])],
+            remove=True)
+
+        volumes = [
+            "{}:/output/".format(host_out_dir),
+            "{}:/input/".format(host_out_path)
+        ]
+        # replace with subprocess
+        self._docker.run(
+            self._config["psrchive_params"]["image"],
+            self._config["psrchive_params"]["cmd"],
+            detach=True,
+            name="psrchive",
+            cpuset_cpus="2",
+            working_dir="/dev/shm",
+            volumes=volumes)
+
+        ####################
+        # Start up UDP2DB
+        ####################
+        cmd = ("LD_PRELOAD=libvma.so taskset -c 1 udp2db "
+               "-k {key} {args} -s {tobs} -H {headerfile}").format(
+            key=self._dada_key,
+            args=self._config["udp2db_params"]["args"],
+            tobs=sensors["time-remaining"],
+            headerfile=dada_header_file.name)
+        cmd = 'bash -c "{cmd}"'.format(cmd=cmd)
+        log.debug("Running command: {0}".format(cmd))
+        # replace with subprocess
+        self._docker.run(
+            self._config["udp2db_params"]["image"],
+            cmd,
+            cap_add=["ALL"],
+            cpu_shares=262144,
+            cpuset_cpus="1",
+            detach=True,
+            volumes=self._volumes,
+            environment={"VMA_MTU": 9000},
+            name="udp2db",
+            ipc_mode="host",
+            network_mode="host",
+            requires_vma=True,
+            ulimits=self.ulimits)
+
+    def stop(self):
+        for name in ["dspsr", "udp2db", "psrchive"]:
+            container = self._docker.get(name)
+            try:
+                log.debug(
+                    "Stopping {name} container".format(
+                        name=container.name))
+                container.kill()
+            except APIError:
+                pass
+            try:
+                log.debug(
+                    "Removing {name} container".format(
+                        name=container.name))
+                container.remove()
+            except BaseException:
+                pass
+
+    def deconfigure(self):
+        log.debug("Destroying dada buffer")
+        cmd = "dada_db -d -k {0}".format(self._dada_key)
+        log.debug("Running command: {0}".format(cmd))
+        # replace with subprocess
+        self._docker.run("psr-capture", cmd, remove=True, ipc_mode="host")
