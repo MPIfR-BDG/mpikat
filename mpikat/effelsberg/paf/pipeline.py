6666fba74bc6c86aa5020341a90da4d0830379a0
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-02-01 17:55:44 +0100

    testing

--- mpikat/effelsberg/paf/pipeline.py
+++ mpikat/effelsberg/paf/pipeline.py
@@ -337,11 +337,6 @@ class Pipeline(object):
     def sensors(self):
         return self._sensors
 
-    @sensors.setter
-    def sensors(self):
-        #self._state = value
-        self.notify()
-
     def _acquire_beam_index(self, ip, port, ndf_check_chk):
         """
         To get the beam ID
