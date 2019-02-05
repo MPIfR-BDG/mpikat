731ec9646db6d82598f2ffef70ca1ae79cb4aef0
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-02-05 14:56:39 +0100

    testing

--- mpikat/effelsberg/edd/pipeline/pipeline.py
+++ mpikat/effelsberg/edd/pipeline/pipeline.py
@@ -80,7 +80,7 @@ def register_pipeline(name):
 
 def safe_popen(cmd, *args, **kwargs):
     if RUN == True:
-        process = Popen(cmd, stdout=PIPE, **kwargs)
+        process = Popen(cmd, stdout=PIPE, *args,**kwargs)
     else:
         process = None
     return process
