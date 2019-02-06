28597d198a1498416a0bce373e00101f0f435ab4
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-02-06 14:41:21 +0100

    testing

--- mpikat/effelsberg/edd/pipeline/pipeline.py
+++ mpikat/effelsberg/edd/pipeline/pipeline.py
@@ -231,7 +231,7 @@ class Mkrecv2Db2Dspsr(object):
         log.debug("Running command: {0}".format(cmd))
         #self._create_ring_buffer = safe_popen(cmd, stdout=PIPE)
         #self._create_ring_buffer.wait()
-        self._create_ring_buffer = ExecuteCommand(cmd, resident=True)
+        self._create_ring_buffer = ExecuteCommand(cmd, resident=False)
         self._create_ring_buffer.stdout_callbacks.add(
                 self._decode_capture_stdout)
         self._destory_ring_buffer.set_finish_event()
