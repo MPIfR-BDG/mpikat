e1b73df96fbce5e80000be96531d545c6b04e655
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-02-13 12:21:25 +0100

    testing

--- mpikat/effelsberg/edd/pipeline/pipeline.py
+++ mpikat/effelsberg/edd/pipeline/pipeline.py
@@ -165,7 +165,9 @@ class ExecuteCommand(object):
                 # print "trying to assign the stdout"
                 stdout = self._process.stdout.readline().rstrip("\n\r")
                 if stdout != b"":
-                    if (not stdout.startswith("heap")) & (not stdout.startswith("mark")) & (not stdout.startswith("[")):
+                    if (not stdout.startswith("heap")) & (not stdout.startswith("mark"))
+                    & (not stdout.startswith("[")) &(not stdout.startswith("-> parallel"))
+                    & (not stdout.startswith("-> sequential")):
                         self.stdout = stdout
                     # print self.stdout, self._command
 
