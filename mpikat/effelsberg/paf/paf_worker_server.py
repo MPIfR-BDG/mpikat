3c11f8ad701eda60dd57f31a24e9a2b966527909
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-02-21 15:28:14 +0100

    update paf_worker_server

--- mpikat/effelsberg/paf/paf_worker_server.py
+++ mpikat/effelsberg/paf/paf_worker_server.py
@@ -1,3 +1,24 @@
+"""
+Copyright (c) 2019 Jason Wu <jwu@mpifr-bonn.mpg.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 import logging
 import tornado
 import coloredlogs
@@ -289,7 +310,6 @@ class PafWorkerServer(AsyncDeviceServer):
 
     @coroutine
     def stop_pipeline(self):
-        self._pipeline_sensor_status.set_value("stopping")
         try:
             self._pipeline_instance.stop()
         except Exception as error:
@@ -342,7 +362,7 @@ def main():
         fmt="[ %(levelname)s - %(asctime)s - %(name)s - %(filename)s:%(lineno)s] %(message)s",
         level=opts.log_level.upper(),
         logger=logger)
-    logging.getLogger('katcp').setLevel('INFO')
+    logging.getLogger('mpikat').setLevel('INFO')
     ioloop = tornado.ioloop.IOLoop.current()
     log.info("Starting PafWorkerServer instance")
     server = PafWorkerServer(opts.host, opts.port)
