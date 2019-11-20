10020a3157b0ddb4b782659092a24e85508e6c4e
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-11-20 17:12:04 +0100

    pipeline update

--- mpikat/effelsberg/edd/pipeline/pipeline.py
+++ mpikat/effelsberg/edd/pipeline/pipeline.py
@@ -332,8 +332,8 @@ class EddPulsarPipeline(AsyncDeviceServer):
         self.ip = ip
         #self._managed_sensors = []
         self.callbacks = set()
-        #self._state = "idle"
-        self._sensors = []
+        self._state = "idle"
+        #self._sensors = []
         self._volumes = ["/tmp/:/scratch/"]
         self._dada_key = None
         self._config = None
