f941da8667501c449ed5639175283f68b5d0ba0d
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-12-02 16:50:17 +0100

    pipeline update

--- mpikat/effelsberg/edd/pipeline/commander.py
+++ mpikat/effelsberg/edd/pipeline/commander.py
@@ -280,11 +280,11 @@ class EddCommander(AsyncDeviceServer):
         self.test_object = self.get_sensor(sensor_value[0].replace("-", "_"))
         log.debug("{} {}".format(sensor_value[0].replace("-", "_"), sensor_value[1]))
         self.test_object.set_value(str(sensor_value[1]))
-            if _observing == 'TRUE':
-                log.debug("observing sensor value is {}".format(self._oberving.get_value()))
-                #log.debug(")
-            elif _observing == 'FALSE':
-                log.debug("observing sensor value is {}".format(self._oberving.get_value()))
+        if _observing == 'TRUE':
+            log.debug("observing sensor value is {}".format(self._oberving.get_value()))
+            #log.debug(")
+        elif _observing == 'FALSE':
+            log.debug("observing sensor value is {}".format(self._oberving.get_value()))
 
     def new_sensor(self, sensor_name, callback):
         #log.debug('New sensor reporting = {}'.format(str(sensor_name)))
