5a9820bf7b50e6134ba67106763d32a703c4ac84
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-12-03 15:38:00 +0100

    pipeline update

--- mpikat/effelsberg/edd/pipeline/pipeline.py
+++ mpikat/effelsberg/edd/pipeline/pipeline.py
@@ -1044,7 +1044,7 @@ class EddPulsarPipeline(AsyncDeviceServer):
             cmd = "numactl -m {numa} dspsr -L 1 -r -minram 1024 {nchan} {nbin} -cpu {cpus} -cuda {cuda_number} -c 1 -D 0.0001 {keyfile}".format(
                 numa=self.numa_number,
                 args=self._config["dspsr_params"]["args"],
-                nchan="-F {}D".format(self.nchannels),
+                nchan="-F {}:D".format(self.nchannels),
                 nbin="-b {}".format(self.nbins),
                 cpus=cpu_numbers,
                 cuda_number=cuda_number,
