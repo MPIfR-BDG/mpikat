82e7525733a83b77660ed1876b0940af537c2781
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-12-05 16:27:53 +0100

    pipeline update

--- mpikat/effelsberg/edd/pipeline/commander.py
+++ mpikat/effelsberg/edd/pipeline/commander.py
@@ -237,7 +237,7 @@ class EddCommander(AsyncDeviceServer):
                 self.first_true = False
                 self.last_value = True
                 self._edd_pipeline_focus.req.start(json_string)
-                #time.sleep(10)
+                time.sleep(5)
                 self._edd_pipeline_faraday.req.start(json_string)
 
             elif bool(self._observing.value() == 'False') & bool(self.last_value == True):
