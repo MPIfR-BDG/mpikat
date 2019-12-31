7494cfd5b5bd07ecd449b65b9739bf69b3fcfc8f
Jason Wu <jwu@mpifr-bonn.mpg.de>
2019-12-31 17:17:40 +0100

    pipeline for roach2 output

--- mpikat/effelsberg/edd/pipeline/pipeline_roach_400mhz.py
+++ mpikat/effelsberg/edd/pipeline/pipeline_roach_400mhz.py
@@ -61,12 +61,12 @@ CONFIG = {
     },
     "dada_db_params":
     {
-        "args": "-n 32 -b 327680000 -p -l",
+        "args": "-n 64 -b 32768000 -p -l",
         "key": "dada"
     },
     "dadc_db_params":
     {
-        "args": "-n 32 -b 327680000 -p -l",
+        "args": "-n 64 -b 32768000 -p -l",
         "key": "dadc"
     },
     "dada_header_params":
