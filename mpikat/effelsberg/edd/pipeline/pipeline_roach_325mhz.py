cc7a57cb848d1107e41694bdd4564106cb8e7ed6
Jason Wu <jwu@mpifr-bonn.mpg.de>
2020-01-04 17:30:11 +0100

    pipeline for roach2 output

--- mpikat/effelsberg/edd/pipeline/pipeline_roach_325mhz.py
+++ mpikat/effelsberg/edd/pipeline/pipeline_roach_325mhz.py
@@ -966,7 +966,7 @@ class EddPulsarPipeline(AsyncDeviceServer):
         try:
          	in_path = os.path.join("/media/scratch/jason/dspsr_output/", tdate,
          	                       self.source_name, str(self.frequency_mhz), tstr, "raw_data")
-         	log.debug(in_path)
+         	#log.debug(in_path)
 			out_path = os.path.join("/media/scratch/jason/dspsr_output/", tdate,
 			                        self.source_name, str(self.frequency_mhz), tstr, "combined_data")
 			log.debug(out_path)
