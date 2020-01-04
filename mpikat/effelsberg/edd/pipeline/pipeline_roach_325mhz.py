7aeb91f81a817c17140a35d3674e1004b3381767
Jason Wu <jwu@mpifr-bonn.mpg.de>
2020-01-04 17:27:42 +0100

    pipeline for roach2 output

--- mpikat/effelsberg/edd/pipeline/pipeline_roach_325mhz.py
+++ mpikat/effelsberg/edd/pipeline/pipeline_roach_325mhz.py
@@ -963,10 +963,10 @@ class EddPulsarPipeline(AsyncDeviceServer):
         ####################################################
         try:
          	in_path = os.path.join("/media/scratch/jason/dspsr_output/", tdate, self.source_name, str(self.frequency_mhz), tstr, "raw_data")
-         	log.debug(in_path)
-         	out_path = os.path.join("/media/scratch/jason/dspsr_output/", tdate, self.source_name, str(self.frequency_mhz), tstr, "combined_data")
-            log.debug(out_path)
-            self.out_path = out_path
+			log.debug(in_path)
+			out_path = os.path.join("/media/scratch/jason/dspsr_output/", tdate, self.source_name, str(self.frequency_mhz), tstr, "combined_data")
+			log.debug(out_path)
+			self.out_path = out_path
             log.debug("Creating directories")
             cmd = "mkdir -p {}".format(in_path)
             log.debug("Command to run: {}".format(cmd))
