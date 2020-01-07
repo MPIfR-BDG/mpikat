88f757637d32152aa3dde59b076ef7993680f78e
Jason Wu <jwu@mpifr-bonn.mpg.de>
2020-01-07 14:33:54 +0100

    pipeline for roach2 output

--- mpikat/effelsberg/edd/pipeline/pipeline.py
+++ mpikat/effelsberg/edd/pipeline/pipeline.py
@@ -1072,7 +1072,7 @@ class EddPulsarPipeline(AsyncDeviceServer):
         #STARTING DSPSR                                    #
         ####################################################
         os.chdir(in_path)
-
+        """
         if (parse_tag(self.source_name) == "default") & self.pulsar_flag:
             cmd = "numactl -m {numa} dspsr {args} {nchan} {nbin} -fft-bench -x 1024 -cpu {cpus} -cuda {cuda_number} -P {predictor} -N {name} -E {parfile} {keyfile}".format(
                 numa=self.numa_number,
@@ -1102,8 +1102,8 @@ class EddPulsarPipeline(AsyncDeviceServer):
         else:
             error = "source is unknown"
             raise EddPulsarPipelineError(error)
-
-        #cmd = "numactl -m {} dbnull -k dadc".format(self.numa_number)
+        """
+        cmd = "numactl -m {} dbnull -k dadc".format(self.numa_number)
         log.debug("Running command: {0}".format(cmd))
         log.info("Staring DSPSR")
         self._dspsr = ExecuteCommand(cmd, outpath=None, resident=True)
