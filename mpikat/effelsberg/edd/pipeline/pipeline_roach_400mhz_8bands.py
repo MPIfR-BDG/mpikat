54d092d7c00f52058d49c49746f9611b5b35119d
Jason Wu <jwu@mpifr-bonn.mpg.de>
2020-01-10 10:52:35 +0100

    pipeline for roach2 output

--- mpikat/effelsberg/edd/pipeline/pipeline_roach_400mhz_8bands.py
+++ mpikat/effelsberg/edd/pipeline/pipeline_roach_400mhz_8bands.py
@@ -1042,8 +1042,8 @@ class EddPulsarPipeline(AsyncDeviceServer):
         log.debug("{}".format(
             (parse_tag(self.source_name) == "default") & self.pulsar_flag))
         if (parse_tag(self.source_name) == "default") & is_accessible('/tmp/epta/{}.par'.format(self.source_name[1:])):
-            cmd = 'numactl -m {} taskset -c {} tempo2 -f /tmp/epta/{}.par -pred "Effelsberg {} {} {} {} 24 12 3599.999999999"'.format(
-                self.numa_number, NUMA_MODE[self.numa_number][1], self.source_name[1:], Time.now().mjd - 2, Time.now().mjd + 2, float(self.frequency_mhz) - 200.0, float(self.frequency_mhz) + 200.0)
+            cmd = 'numactl -m {} taskset -c {} tempo2 -f /tmp/epta/{}.par -pred "Effelsberg {} {} {} {} 24 2 3599.999999999"'.format(
+                self.numa_number, NUMA_MODE[self.numa_number][1], self.source_name[1:], Time.now().mjd - 1, Time.now().mjd + 1, float(self.frequency_mhz) - 200.0, float(self.frequency_mhz) + 200.0)
             log.debug("Command to run: {}".format(cmd))
             self.tempo2 = ExecuteCommand(cmd, outpath=None, resident=False)
             self.tempo2_pid = self.tempo2.pid
