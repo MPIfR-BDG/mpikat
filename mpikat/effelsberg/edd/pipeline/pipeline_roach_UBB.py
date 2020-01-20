d69e889132e71feba691f4063df8a28a2e87bd15
Jason Wu <jwu@mpifr-bonn.mpg.de>
2020-01-20 15:58:23 +0100

    pipeline for roach2 output

--- mpikat/effelsberg/edd/pipeline/pipeline_roach_UBB.py
+++ mpikat/effelsberg/edd/pipeline/pipeline_roach_UBB.py
@@ -61,12 +61,12 @@ CONFIG = {
     },
     "dada_db_params":
     {
-        "args": "-n 16 -b 1048576000 -p -l",
+        "args": "-n 32 -b 1048576000 -p -l",
         "key": "dada"
     },
     "dadc_db_params":
     {
-        "args": "-n 16 -b 1048576000 -p -l",
+        "args": "-n 32 -b 1048576000 -p -l",
         "key": "dadc"
     },
     "dada_header_params":
