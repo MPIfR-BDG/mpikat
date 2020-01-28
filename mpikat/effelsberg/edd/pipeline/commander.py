1e75110e356e85a51616b165920bb5b0531ce03c
Jason Wu <jwu@mpifr-bonn.mpg.de>
2020-01-28 13:51:52 +0100

    pipeline for roach2 output

--- mpikat/effelsberg/edd/pipeline/commander.py
+++ mpikat/effelsberg/edd/pipeline/commander.py
@@ -267,7 +267,7 @@ class EddCommander(AsyncDeviceServer):
                 if self._source.value().split("_")[1] == "2K":
                     log.debug("making JSON for mode = {}".format(self._source.value().split("_")[1]))
 
-                    json_string_numa0 = json.dumps({"source-name": "{}".format(self._source.value().split("_")[1]), "nchannels": 4096, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":1})
+                    json_string_numa0 = json.dumps({"source-name": "{}".format(self._source.value().split("_")[0]), "nchannels": 4096, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":1})
                     json_string_numa1 = json.dumps({"source-name": "{}".format(self._source.value()), "nchannels": 4096, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":0})
                     #log.debug("{}".format({"source-name": "{}_{}".format(self._source.value().split("_")[0], self._source.value().split("_")[2]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band": 1}))
                     #json_string_numa0 = json.dumps({"source-name": "{}_{}".format(self._source.value().split("_")[0], self._source.value().split("_")[2]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band": 1})
