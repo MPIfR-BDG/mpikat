1f6b880a80eb826bd77688f2d90633d80f392e10
Jason Wu <jwu@mpifr-bonn.mpg.de>
2020-01-28 14:17:12 +0100

    pipeline for roach2 output

--- mpikat/effelsberg/edd/pipeline/commander.py
+++ mpikat/effelsberg/edd/pipeline/commander.py
@@ -270,11 +270,11 @@ class EddCommander(AsyncDeviceServer):
                     #log.debug("json_source_name= {}".format(json_source_name))
                     if len(self._source.value().split("_"))== 3:
 
-                        json_string_numa0 = json.dumps({"source-name": "{}_{}".format(json_source_name[0], json_source_name[2]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":1})
-                        json_string_numa1 = json.dumps({"source-name": "{}_{}".format(json_source_name[0], json_source_name[2]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":0})
+                        json_string_numa0 = json.dumps({"source-name": "{}_{}".format(self._source.value().split("_")[0], self._source.value().split("_")[2]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":1})
+                        json_string_numa1 = json.dumps({"source-name": "{}_{}".format(self._source.value().split("_")[0], self._source.value().split("_")[2]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":0})
                     else :
-                        json_string_numa0 = json.dumps({"source-name": "{}_{}".format(json_source_name[0]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":1})
-                        json_string_numa1 = json.dumps({"source-name": "{}_{}".format(json_source_name[0]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":0})
+                        json_string_numa0 = json.dumps({"source-name": "{}".format(self._source.value().split("_")[0]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":1})
+                        json_string_numa1 = json.dumps({"source-name": "{}".format(self._source.value().split("_")[0]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band":0})
                     #log.debug(json_string_numa0)
                     #log.debug(json_string_numa1)
                     #log.debug("{}".format({"source-name": "{}_{}".format(self._source.value().split("_")[0], self._source.value().split("_")[2]), "nchannels": 1024, "nbins": 1024, "ra": self._ra.value(), "dec": self._dec.value(), "band": 1}))
