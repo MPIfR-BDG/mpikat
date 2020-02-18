deffe6c6182c812b2003d23eff6a5e2a41b4224c
Jason Wu <jwu@mpifr-bonn.mpg.de>
2020-02-18 11:47:54 +0100

    new png handling method

--- mpikat/effelsberg/edd/pipeline/archive_directory_monitor_EDD.py
+++ mpikat/effelsberg/edd/pipeline/archive_directory_monitor_EDD.py
@@ -110,12 +110,11 @@ class ArchiveAdder(FileSystemEventHandler):
                 log.info(
                     "Passing archive file {} for processing".format(fname[0:-9]))
                 self.process(fname[0:-9])
-            #imageFile = open("{}/fscrunch.png".format(self.output_dir), "rb")
-            #log.info("reading fscrunch.png")
-            #fscrunch = base64.b64encode(imageFile.read())
-
-            yield self._png_server.until_synced()
-            self._png_server.req.fscrunch(fscrunch)
+            imageFile = open("{}/fscrunch.png".format(self.output_dir), "rb")
+            log.info("reading fscrunch.png")
+            fscrunch = base64.b64encode(imageFile.read())
+            yield png_server.until_synced()
+            png_server.req.fscrunch(fscrunch)
         except Exception as error:
             log.error(error)
 
