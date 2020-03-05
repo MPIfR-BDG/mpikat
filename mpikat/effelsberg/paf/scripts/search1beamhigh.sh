0ff8ae3ae92f9fa89f560b573a082f46561a8347
Jason Wu <jwu@mpifr-bonn.mpg.de>
2020-03-05 15:55:36 +0100

    update test scripts

--- mpikat/effelsberg/paf/scripts/search1beamhigh.sh
+++ mpikat/effelsberg/paf/scripts/search1beamhigh.sh
@@ -1,9 +1,9 @@
-python /mpikat/mpikat/core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDNBEAMS 18" -t 20
-python /mpikat/mpikat/core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDBANDOFFSET 0" -t 20
-python /mpikat/mpikat/core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDNBANDS 48" -t 20
-python /mpikat/mpikat/core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDFREQUENCY 1340.5" -t 20
-python /mpikat/mpikat/core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDMODE search1beamhigh" -t 20
-python /mpikat/mpikat/core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDZOOMBAND0 33" -t 20
-python /mpikat/mpikat/core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDZOOMNBANDS 5" -t 20
-python /mpikat/mpikat/core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDWRITEFIL 1" -t 20
-python /mpikat/mpikat/core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CONFIGURE" -t 200
+python ../../../core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDNBEAMS 18" -t 20
+python ../../../core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDBANDOFFSET 0" -t 20
+python ../../../core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDNBANDS 48" -t 20
+python ../../../core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDFREQUENCY 1340.5" -t 20
+python ../../../core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDMODE search1beamhigh" -t 20
+python ../../../core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDZOOMBAND0 33" -t 20
+python ../../../core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDZOOMNBANDS 5" -t 20
+python ../../../core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CMDWRITEFIL 1" -t 20
+python ../../../core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:CONFIGURE" -t 200
--- mpikat/effelsberg/paf/scripts/start_scpi.sh
+++ mpikat/effelsberg/paf/scripts/start_scpi.sh
@@ -1,1 +1,1 @@
-python /mpikat/mpikat/core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:START" -t 200
+python ../../../core/scpi_client.py -H 134.104.64.51 -p 5004 -m "PAFBE:START" -t 200
